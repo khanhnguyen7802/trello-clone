d2a88e5968826a0520d64232ac02fd3123739eae
khanhnguyen <khanhnguyen7802@gmail.com>
2024-06-29 16:20:02 +0200

    theme setting

---
+++ trello-web/src/theme.js
@@ -0,0 +1,24 @@
+// use this file to configure the theme
+
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    text: {
+      secondary: purple[500], 
+    }
+  },
+});
+
+export default theme;
